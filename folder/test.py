377c9dbd0629cbfa54a3763790a58f6e8bd7259c
Umang <82362300+Umang-2@users.noreply.github.com>
2022-04-24 23:11:01 +0530

    Update test.py

--- folder/test.py
+++ folder/test.py
@@ -13,8 +13,12 @@ df = pd.read_csv(path_df)
 st.write(df)
 
 path_np_1 = Path(__file__).parents[0]/ 'reverse_target_word_index.npy'
+path_np_2 = Path(__file__).parents[0]/ 'target_word_index.npy'
+path_np_3 = Path(__file__).parents[0]/ 'reverse_source_word_index.npy'
 reverse_target_word_index = np.load(path_np_1,allow_pickle='TRUE').item()
-# target_word_index = np.load('target_word_index.npy',allow_pickle='TRUE').item()
-# reverse_source_word_index = np.load('reverse_source_word_index.npy',allow_pickle='TRUE').item()
+target_word_index = np.load(path_np_2,allow_pickle='TRUE').item()
+reverse_source_word_index = np.load(path_np_3,allow_pickle='TRUE').item()
 
 st.write(reverse_target_word_index[1])
+st.write(target_word_index[1])
+st.write(reverse_source_word_index[1])
