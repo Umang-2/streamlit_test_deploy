ac28903fe658cc014846c0a487698dc64acec0a8
Umang <82362300+Umang-2@users.noreply.github.com>
2022-04-24 17:40:35 +0530

    Update test.py

--- folder/test.py
+++ folder/test.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from pathlib import Path
 import pandas as pd
 # df = pd.read_csv('pytube_eg.csv')
 # st.write(df)
+
 st.write('Test successful!!')
+df = Path(__file__).parents[0]/ 'folder/pytube_eg.csv'
+# 'GarretBurhennData/Garret_Burhenn_Pitches.csv'
+st.write(df)
