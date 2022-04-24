80abf10ebd1bf83ed71bb18ecd277855d9194453
Umang <82362300+Umang-2@users.noreply.github.com>
2022-04-24 17:46:48 +0530

    Update test.py

--- folder/test.py
+++ folder/test.py
@@ -5,7 +5,7 @@ import pandas as pd
 # st.write(df)
 
 st.write('Test successful!!')
-path_df = Path(__file__).parents[0]/ 'folder/pytube_eg.csv'
+path_df = Path(__file__).parents[0]/ 'pytube_eg.csv'
 st.write(path_df)
 # 'GarretBurhennData/Garret_Burhenn_Pitches.csv'
 df = pd.read_csv(path_df)
